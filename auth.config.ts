66a26bb4b551774d8ee99fb975902058e9703f3a
luzumsuzadam <cengizulutas123123@gmail.com>
2025-03-28 18:01:56 +0300

    split auth.ts

--- auth.config.ts
+++ auth.config.ts
@@ -1,41 +1,11 @@
 import type { NextAuthConfig } from "next-auth";
-import Credentials from "next-auth/providers/credentials";
-import { compareSync } from "bcrypt-ts-edge";
-import { getUserByEmail } from "./lib/data/getUser";
+
 // import { cookies } from "next/headers";
 import { NextResponse } from "next/server";
 
 // Notice this is only an object, not a full Auth.js instance
 export default {
-  providers: [
-    Credentials({
-      credentials: {
-        email: { type: "email" },
-        password: { type: "password" },
-      },
-      async authorize(credentials) {
-        const user = await getUserByEmail(credentials.email as string);
-
-        if (user && user.password) {
-          const passwordsMatch = await compareSync(
-            credentials.password as string,
-            user.password
-          );
-          if (passwordsMatch) {
-            return {
-              id: user.id,
-              name: user.name,
-              email: user.email,
-              role: user.role,
-              image: user.image,
-            };
-          }
-        }
-
-        return null;
-      },
-    }),
-  ],
+  providers: [],
   callbacks: {
     authorized({request}) {
       // Check for session cart cookie
--- auth.ts
+++ auth.ts
@@ -1,5 +1,8 @@
 import NextAuth from "next-auth";
 import authConfig from "./auth.config"
+import Credentials from "next-auth/providers/credentials";
+import { compareSync } from "bcrypt-ts-edge";
+import { getUserByEmail } from "./lib/data/getUser";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { prisma } from "@/db/prisma";
 
@@ -35,7 +38,36 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
     },
     
   },
+  providers: [
+    Credentials({
+      credentials: {
+        email: { type: "email" },
+        password: { type: "password" },
+      },
+      async authorize(credentials) {
+        const user = await getUserByEmail(credentials.email as string);
+
+        if (user && user.password) {
+          const passwordsMatch = await compareSync(
+            credentials.password as string,
+            user.password
+          );
+          if (passwordsMatch) {
+            return {
+              id: user.id,
+              name: user.name,
+              email: user.email,
+              role: user.role,
+              image: user.image,
+            };
+          }
+        }
+
+        return null;
+      },
+    }),
+  
+  ],
   session: { strategy: "jwt", maxAge: 30 * 24 * 60 * 60 },
   adapter: PrismaAdapter(prisma),
-  providers: authConfig.providers,
 });
