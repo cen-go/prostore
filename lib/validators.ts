33103f9bf899336105220a5c37efdf05e49ca860
luzumsuzadam <cengizulutas123123@gmail.com>
2025-04-10 16:57:23 +0300

    prisma error

--- lib/validators.ts
+++ lib/validators.ts
@@ -78,3 +78,24 @@ export const paymentMethodSchema = z.object({
     required_error: "You need to select a payment method.",
   }),
 })
+
+// Schema for interting order
+export const insertOrderSchema = z.object({
+  userId: z.string().min(1, "User is required"),
+  itemsPrice: currency,
+  shippingPrice: currency,
+  taxPrice: currency,
+  totalPrice: currency,
+  paymentMethod: z.enum([PaymentMethod.PayPal, PaymentMethod.Stripe, PaymentMethod.CashOnDelivery]),
+  shippingAddress: shippingAddressSchema
+});
+
+// Schema for inserting an order item
+export const insertOrderItemSchema = z.object({
+  productId: z.string(),
+  slug: z.string(),
+  name: z.string(),
+  image: z.string(),
+  price: currency,
+  qnty: z.number(),
+});
--- types/index.ts
+++ types/index.ts
@@ -1,6 +1,6 @@
 import { z } from "zod";
 
-import { insertProductSchema, cartItemSchema, insertCartSchema, shippingAddressSchema } from "@/lib/validators";
+import { insertProductSchema, cartItemSchema, insertCartSchema, shippingAddressSchema, insertOrderItemSchema, insertOrderSchema } from "@/lib/validators";
 
 export type Product = z.infer<typeof insertProductSchema> & {
   id: string;
@@ -13,4 +13,17 @@ export type Cart = z.infer<typeof insertCartSchema>;
 
 export type CartItem = z.infer<typeof cartItemSchema>;
 
-export type ShippingAddress = z.infer<typeof shippingAddressSchema>; 
+export type ShippingAddress = z.infer<typeof shippingAddressSchema>;
+
+export type OrderItem = z.infer<typeof insertOrderItemSchema>;
+export type Order = z.infer<typeof insertOrderSchema> & {
+  id: string;
+  paymentResult?: JSON;
+  isPaid: boolean;
+  paidAt: Date | null;
+  isDelivered: boolean;
+  daliveredAt: Date | null;
+  createdAt: Date;
+  orderItems: OrderItem[];
+  user: {name: string; email: string;};
+};
